886ae13e93de68f9b1dd90513a3fb03624a6eb57
Iwona Kulacz <ikulacz@cleeng.com>
2021-06-25 08:59:52 +0200

    integrate with cleeng checkout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Login, Register, OfferContainer } from 'mediastoresdk-iwona';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 
-function App() {
+const history = createBrowserHistory();
+const App = () => {
+  const [isPopupOpened, setIsPopupOpened] = useState(false);
+  const [popupType, setPopupType] = useState('');
+ 
   return (
     <div className="App">
+      <Router history={history}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul className="menu">
+          <li><button onClick={() => {setIsPopupOpened(true); setPopupType('login')}}>Login</button></li>
+          <li><button onClick={() => {setIsPopupOpened(true); setPopupType('register')}}>Register</button></li>
+        </ul>
       </header>
+      <main>
+      <button onClick={() => {setIsPopupOpened(true); setPopupType('checkout')}}>Buy</button>
+        {isPopupOpened && (
+          <div className="popupWrapper" onClick={() => {
+              setIsPopupOpened(false);
+            }}>
+            <div className="popupContent" onClick={(event) => {
+              event.stopPropagation(); 
+            }}>
+              {popupType === 'login' && (
+                <Login urlProps={{location: {search: 'offerId=S343557015_PL'}}}/>
+
+              )}
+              {popupType === 'register' && (
+                <Register urlProps={{location: {search: 'offerId=S343557015_PL'}}}/>
+
+              )}
+              {popupType === 'checkout' && (
+                <OfferContainer urlProps={{location: {search: 'offerId=S343557015_PL'}}}/>
+              )}
+            </div>
+          </div>
+        )}
+      </main>
+      </Router>
     </div>
   );
 }
